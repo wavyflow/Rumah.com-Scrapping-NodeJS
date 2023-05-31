1467521e62de91363ba78eebab3301ccfa6895e0
Ashary Vermaysha <avermaysha@gmail.com>
2023-05-31 08:28:10 +0700

    Update Scrape.crawl to use #contextual-navigation

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ async function main(url: string, multibar: cliProgress.MultiBar) {
       return false;
     }
 
-    const content = await Scrape.crawl(val.link, ".navbar-brand");
+    const content = await Scrape.crawl(val.link, "#contextual-navigation");
 
     if (!content) {
       return null;
